e9b6350385b9a90db30efab5d3d929e4df949b47
Lukasz Bros <lukasz.bros@xebia.com>
2023-05-30 14:33:44 +0200

    simple component generation

---
+++ src/component/index.ts
@@ -0,0 +1,32 @@
+import {
+  apply,
+  chain,
+  MergeStrategy,
+  mergeWith,
+  move,
+  Rule,
+  SchematicContext,
+  template,
+  Tree,
+  url
+} from '@angular-devkit/schematics';
+import { join, normalize } from 'path';
+
+export async function setupOptions(host: Tree, options: any): Promise<Tree> {
+  options.path = join(normalize('output/components'));
+  return host;
+}
+
+export function generateComponent(_options: any): Rule {
+  return async (tree: Tree, _context: SchematicContext) => {
+    await setupOptions(tree, _options);
+
+    const movePath = normalize(_options.path + '/');
+    const templateSource = apply(url('./files'), [
+      template({..._options}),
+      move(movePath)
+    ]);
+
+    return chain([mergeWith(templateSource, MergeStrategy.Overwrite)]);
+  };
+}
